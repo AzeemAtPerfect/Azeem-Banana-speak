9cf18439d70d7bc920b82d505624a6bbc4acd0d1
Azeem <perfectdesigns001@gmail.com>
2023-03-19 15:06:22 +0500

    get input in js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Working")
+const btnTranslate=document.getElementById("btn-translate")
+const txtInput = document.getElementById("txt-input")
+
+// Event Listner
+btnTranslate.addEventListener("click",getInput)
+
+// Functions
+function getInput(){
+    console.log(txtInput.value)
+}
