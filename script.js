8bc423464a9d1d0fa678c2e57d29a23234da0643
Azeem <perfectdesigns001@gmail.com>
2023-03-20 19:21:54 +0500

    done

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const btnTranslate=document.getElementById("btn-translate")
+const btnTranslate = document.getElementById("btn-translate")
 const txtInput = document.getElementById("txt-input")
+const txtTranslated = document.querySelector(".txt-translated")
+
+const url = "https://api.funtranslations.com/translate/minion.json"
+
+
+
 
 // Event Listner
-btnTranslate.addEventListener("click",getInput)
+btnTranslate.addEventListener("click", getTranslation)
 
 // Functions
-function getInput(){
-    console.log(txtInput.value)
+function getApiURL(text) {
+    return url + "?" + "text=" + text
+}
+
+function getTranslation() {
+    const inputText = txtInput.value
+    // console.log(getApiURL(inputText))
+    fetch(getApiURL(inputText)).then(response => (response.json())).then(j => (txtTranslated.innerText = j.contents.translated))
 }
